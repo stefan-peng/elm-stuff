72fde34f7b7f0991a8e120bd406a9b8c1677e3c6
Stefan Peng <stefanpeng9@gmail.com>
2019-08-09 10:39:48 -0400

    Initial commit

---
+++ src/piechart.elm
@@ -0,0 +1,84 @@
+module PieChart exposing (main)
+
+import Array exposing (Array)
+import Color exposing (Color)
+import Path
+import Shape exposing (defaultPieConfig)
+import TypedSvg exposing (g, svg, text_)
+import TypedSvg.Attributes exposing (dy, fill, stroke, textAnchor, transform, viewBox)
+import TypedSvg.Attributes.InPx exposing (height, width)
+import TypedSvg.Core exposing (Svg, text)
+import TypedSvg.Types exposing (AnchorAlignment(..), Fill(..), Transform(..), em)
+
+
+w : Float
+w =
+    990
+
+
+h : Float
+h =
+    504
+
+
+colors : Array Color
+colors =
+    Array.fromList
+        [ Color.rgb255 152 171 198
+        , Color.rgb255 138 137 166
+        , Color.rgb255 123 104 136
+        , Color.rgb255 107 72 107
+        , Color.rgb255 159 92 85
+        , Color.rgb255 208 116 60
+        , Color.rgb255 255 96 0
+        ]
+
+
+radius : Float
+radius =
+    min w h / 2
+
+
+view : List ( String, Float ) -> Svg msg
+view model =
+    let
+        pieData =
+            model |> List.map Tuple.second |> Shape.pie { defaultPieConfig | outerRadius = radius }
+
+        makeSlice index datum =
+            Path.element (Shape.arc datum) [ fill <| Fill <| Maybe.withDefault Color.black <| Array.get index colors, stroke Color.white ]
+
+        makeLabel slice ( label, value ) =
+            let
+                ( x, y ) =
+                    Shape.centroid { slice | innerRadius = radius - 40, outerRadius = radius - 40 }
+            in
+            text_
+                [ transform [ Translate x y ]
+                , dy (em 0.35)
+                , textAnchor AnchorMiddle
+                ]
+                [ text label ]
+    in
+    svg [ viewBox 0 0 w h ]
+        [ g [ transform [ Translate (w / 2) (h / 2) ] ]
+            [ g [] <| List.indexedMap makeSlice pieData
+            , g [] <| List.map2 makeLabel pieData model
+            ]
+        ]
+
+
+data : List ( String, Float )
+data =
+    [ ( "/notifications", 1 )
+    , ( "/about", 2 )
+    , ( "/product", 2 )
+    , ( "/blog", 2 )
+    , ( "/shop", 2 )
+    , ( "/profile", 2 )
+    , ( "/", 2 )
+    ]
+
+
+main =
+    view data
